02cdf316a9cdd5a383b23627493e7883823680e8
Stepanov Vadim <vadgat777@mail.ru>
2022-02-04 18:21:13 +0700

    four commit

---
+++ Scripts/Bullet.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    [SerializeField] private int _damage;
+    [SerializeField] private float _speed;
+
+    void Update()
+    {
+        transform.Translate(Vector2.left * _speed * Time.deltaTime, Space.World);
+    }
+}
---
+++ Scripts/Weapon/Pistol.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class Pistol : Weapon
+{
+    public override void Shoot(Transform shootPoint)
+    {
+        Instantiate(Bullet, shootPoint.position, Quaternion.identity);
+    }
+}
