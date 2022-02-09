e0fc63b2ca0f14f143004261824a7c8fb6e0a4ca
Stepanov Vadim <vadgat777@mail.ru>
2022-02-09 17:57:50 +0700

    eight commit

--- Scripts/Enemy/Enemy.cs
+++ Scripts/Enemy/Enemy.cs
@@ -6,11 +6,16 @@ public class Enemy : MonoBehaviour
     [SerializeField] private int _health;
     [SerializeField] private int _reward;
 
-    [SerializeField] private Player _target;
+    private Player _target;
 
     public Player Target => _target;
 
     public event UnityAction Dying;
+
+    public void Init(Player target)
+    {
+        _target = target;
+    }
 
     public void TakeDamage(int damage)
     {
---
+++ Scripts/Spawner.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    [SerializeField] private List<Wave> _waves;
+
+}
+
+[System.Serializable]
+public class Wave
+{
+    public GameObject Template;
+    public float Delay;
+    public int Count;
+}
