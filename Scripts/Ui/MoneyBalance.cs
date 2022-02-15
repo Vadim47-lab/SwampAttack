7419fd3f3a83ef1c3f8caceace48141ff3849bf0
Stepanov Vadim <vadgat777@mail.ru>
2022-02-15 15:07:26 +0700

    13 commit

---
+++ Scripts/Ui/MoneyBalance.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class MoneyBalance : MonoBehaviour
+{
+    [SerializeField] private TMP_Text _money;
+    [SerializeField] private Player _player;
+
+    private void OnEnable()
+    {
+        _money.text = "Деньги = ";
+        _money.text = _player.Money.ToString();
+    }
+}
