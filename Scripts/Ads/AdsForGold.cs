444c65b21e1db5f3c9e988d4864614402cc64581
Stepanov Vadim <vadgat777@mail.ru>
2022-02-22 17:21:29 +0700

    17 commit

---
+++ Scripts/Ads/AdsForGold.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine.Advertisements;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class AdsForGold : MonoBehaviour, IUnityAdsListener
+{
+    [SerializeField] private GameObject _startWatchingButton;
+
+    private readonly string _gameId = "4626021";
+    private readonly string _myPlacementId = "rewaredVideo";
+    private readonly bool _testMode = true;
+
+    private void Start()
+    {
+        Advertisement.AddListener(this);
+        Advertisement.Initialize(_gameId, _testMode);
+
+        _startWatchingButton.GetComponent<Button>().onClick.AddListener(() =>
+        {
+            Advertisement.Show(_myPlacementId);
+            _startWatchingButton.SetActive(false);
+        });
+    }
+
+    public void OnUnityAdsDidFinish(string placementId, ShowResult showResult)
+    {
+        if (showResult == ShowResult.Finished)
+        {
+            Debug.Log("Вам начисленно 100 монеток!");
+        }
+        else if (showResult == ShowResult.Skipped)
+        {
+
+        }
+        else if (showResult == ShowResult.Failed)
+        {
+               
+        }
+    }
+
+    public void OnUnityAdsReady(string placementId)
+    {
+        if (placementId == _myPlacementId)
+        {
+            _startWatchingButton.SetActive(true);
+        }
+    }
+
+    public void OnUnityAdsDidError(string message)
+    {
+        
+    }
+
+    public void OnUnityAdsDidStart(string placementId)
+    {
+        
+    }
+}
---
+++ Scripts/Ads/AdsInit.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine.Advertisements;
+using System.Collections;
+using UnityEngine;
+
+public class AdsInit : MonoBehaviour
+{
+    private readonly string _gameId = "4626021";
+    private readonly bool _testMode = true;
+
+    private void Start()
+    {
+        Advertisement.Initialize(_gameId, _testMode);
+        StartCoroutine(ShowBannerWhenReady());
+    }
+
+    IEnumerator ShowBannerWhenReady()
+    {
+        while (!Advertisement.IsReady("MainBottom"))
+        {
+            yield return new WaitForSeconds(0.5f);
+        }
+
+        Advertisement.Banner.Show("MainBottom");
+    }
+}
